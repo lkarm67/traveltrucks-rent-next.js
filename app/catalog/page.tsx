2c8ee3f5ad5026b0a665e72f24c3bdb330eb1335
Luidmyla Karmeliuk <lkarm67@gmail.com>
2026-01-07 08:38:41 +0200

    catalog-page

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,4 +1,17 @@
-const CatalogPage = () => {
-    return <div>Catalog</div>;
+import { getCampers } from "@/lib/api";
+
+const CatalogPage = async () => {
+    const campers = await getCampers(1, 10);
+    console.log("campers", campers);
+    return (
+        <div>
+            <h1>Catalog</h1>
+            <ul>
+                {campers.campers.map(camper => (
+                    <li key={camper.id}>{camper.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 export default CatalogPage;
---
+++ lib/api.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+export type Camper = {
+    id: number;
+    name: string;
+    type: string;
+    pricePerDay: number;
+    imageUrl: string;
+    description: string;
+};
+
+export type CamperListResponse = {
+    campers: Camper[];
+    total: number;
+    page: number;
+    pageSize: number;
+    totalPages: number;
+    rating: number;
+    reviews: number;
+    location: string;
+    supportingText: string;
+};
+
+axios.defaults.baseURL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers';
+
+export const getCampers = async (page: number, pageSize: number): Promise<CamperListResponse> => {
+    const res = await axios.get<CamperListResponse>('/', {
+        params: {
+            page,
+            pageSize
+        }
+    });
+    return res.data;
+};
